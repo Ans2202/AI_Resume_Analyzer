8e4b79b7acdf52ae5b77e505a885cd1b8ca45c1e
Anshu Tiwari <96188323+Ans2202@users.noreply.github.com>
2026-05-28 16:59:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ nltk.download("punkt")
 nltk.download('punkt_tab')
 nltk.download("stopwords")
 nltk.download("averaged_perceptron_tagger")
+nltk.download("averaged_perceptron_tagger_eng")  # Fix for NLTK 3.9+ on Streamlit Cloud
 
 # -------------------- PAGE CONFIG --------------------
 
